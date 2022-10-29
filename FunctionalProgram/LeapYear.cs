8d18c0294ddf9484e5948a3adcb64646182d122a
rahul01r <rrdubeynibm@gmail.com>
2022-10-29 23:01:33 +0530

    [rahul] Add:UC2-LeapYear

---
+++ FunctionalProgram/LeapYear.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgram
+{
+    public class LeapYear
+    {
+        public void yearcheck()
+        {
+            int year, n;
+
+
+            Console.WriteLine("enter the year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+            string num = year.ToString();
+            n = num.Length;
+            // CHECK LENGTH CONDITION
+            if (n != 4)
+            {
+                Console.WriteLine("enter year as 4 digit number");
+                return;
+            }
+            // COMPUTATION
+            {
+                if (year % 400 == 0 && year % 100 == 0)
+                    Console.WriteLine("year " + year + " is a leap year");
+
+                if (year % 100 != 0 && year % 4 == 0)
+                    Console.WriteLine("year " + year + " is a leap year");
+
+                else
+                    Console.WriteLine("year " + year + " is not a leap year");
+            }
+        }
+    }
+}
--- FunctionalProgram/Program.cs
+++ FunctionalProgram/Program.cs
@@ -4,8 +4,23 @@
     {
         static void Main(string[] args)
         {
-            FlipCoin flipCoin = new FlipCoin();
-            flipCoin.Coin();
+
+            while (true)
+            {
+                Console.WriteLine("Select program \n1.Flip Coin \n2.Leap Year");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.Coin();
+                    break;
+                    case 2:
+                        LeapYear leapYear = new LeapYear();
+                        leapYear.yearcheck();
+                    break;
+                }
+            }
         }
     }
 }
